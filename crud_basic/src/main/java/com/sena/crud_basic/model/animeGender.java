152255b775a2617b0e356b13a6706dda85ee4e58
Oscar <munozmejiaoscarmauricio@gmail.com>
2025-03-06 11:34:22 -0500

    arreglo

--- crud_basic/src/main/java/com/sena/crud_basic/model/animeGender.java
+++ crud_basic/src/main/java/com/sena/crud_basic/model/animeGender.java
@@ -1,11 +1,20 @@
 package com.sena.crud_basic.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity(name="anime_gender")
 public class AnimeGender {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="anime_gender_id")
+    private int anime_gender_id;
 
     @ManyToOne
     @JoinColumn(name = "gender_id", nullable = false)
@@ -15,9 +24,18 @@ public class AnimeGender {
     @JoinColumn(name = "anime_id", nullable = false)
     private Anime anime;
 
-    public AnimeGender(Gender gender, Anime anime) {
+    public AnimeGender(Gender gender, Anime anime, int anime_gender_id) {
+        this.anime_gender_id = anime_gender_id;
         this.gender = gender;
         this.anime = anime;
+    }
+
+    public void setAnime_gender_id(int anime_gender_id) {
+        this.anime_gender_id = anime_gender_id;
+    }
+
+    public int getAnime_gender_id() {
+        return anime_gender_id;
     }
 
     public void setGender(Gender gender) {
