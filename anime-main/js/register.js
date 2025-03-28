89c1277bedf4d9689094415072e3cce103712e72
Oscar <munozmejiaoscarmauricio@gmail.com>
2025-03-28 11:34:48 -0500

    Registro de Author funcionando, aun no funciona el de pltform

--- anime-main/js/register.js
+++ anime-main/js/register.js
@@ -4,41 +4,64 @@ let headersList = {
     "Content-Type": "application/json"
 }
 
-    async function fetchCountries() {
-        try {
-            let response = await fetch("http://localhost:8085/api/v1/country/", {
-                method: "GET",
-                headers: headersList
+async function fetchCountries() {
+    try {
+        let response = await fetch("http://localhost:8085/api/v1/country/", {
+            method: "GET",
+            headers: headersList 
+        });
+
+        if (response.ok) {
+            let data = await response.json();
+            let datalist = document.getElementById("countryList");
+
+            
+            data.forEach((country) => {
+                let option = document.createElement("option");
+                option.value = country.name;
+                datalist.appendChild(option);
             });
-
-            if (response.ok) {
-                let data = await response.json();
-                let datalist = document.getElementById("countryList");
-
-                // Iterar sobre los datos y añadir opciones al datalist
-                data.forEach((country) => {
-                    let option = document.createElement("option");
-                    option.value = country.name;
-
-                    datalist.appendChild(option);
-                });
-            } else {
-                console.error("Error al obtener datos:", response.status);
-            }
-        } catch (error) {
-            console.error("Hubo un error al cargar los países:", error);
+        } else {
+            console.error("Error al obtener datos:", response.status);
         }
+    } catch (error) {
+        console.error("Hubo un error al cargar los países:", error);
     }
+}
 
-    fetchCountries();
 
+
+let countryID;
+let countryInput = document.getElementById("country");
+countryInput.addEventListener("change", () => {
+    let selectedCountry = countryInput.value;
+    console.log("País seleccionado:", selectedCountry);
     
+    
+    async function selectId() {
+        let response = await fetch("http://localhost:8085/api/v1/country/", {
+            method: "GET",
+            headers: headersList 
+        });
+        let data = await response.json();
 
+        data.forEach((country) => {
+            if (country.name === selectedCountry) {
+                countryID = country.country_id; 
+                console.log("ID del país:", countryID);
+            }
+        });
+    }
+    selectId();
+
+});
+ 
+fetchCountries();
 
 function registerGenre() {
 
     return new Promise(async (resolve) => {
-        // alert("hola");
+
 
         let bodyContent = JSON.stringify({
             "id": 0,
@@ -60,7 +83,7 @@ function registerGenre() {
 function registerCountry() {
 
     return new Promise(async (resolve) => {
-        // alert("hola");
+
 
         let bodyContent = JSON.stringify({
             "id": 0,
@@ -82,13 +105,13 @@ function registerCountry() {
 function registerAuthor() {
 
     return new Promise(async (resolve) => {
-        // alert("hola");
+
 
         let bodyContent = JSON.stringify({
             "id": 0,
             "name": document.getElementById("nameAuthor").value,
             "image": document.getElementById("image").value,
-            "country_id": {"country_id":document.getElementById("countryA").value}
+            "country_id": {"country_id":countryID}
         });
 
         let response = await fetch("http://localhost:8085/api/v1/author/", {
@@ -99,20 +122,20 @@ function registerAuthor() {
 
         let data = await response.text();
         console.log(data);
-
+        alert(selectedCountry);
     });
+    
 }
 
 function registerPlatform() {
 
     return new Promise(async (resolve) => {
-        // alert("hola");
 
         let bodyContent = JSON.stringify({
             "id": 0,
             "name": document.getElementById("namePlatform").value,
             "image": document.getElementById("image").value,
-            "country": document.getElementById("countryP").value
+            "country_id": {"country_id":countryID}
         });
 
         let response = await fetch("http://localhost:8085/api/v1/platform/", {
